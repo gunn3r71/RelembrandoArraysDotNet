7d88658c2f32b7fd2d0d9a2587a2bafa873f19c4
Lucas <lucasgunner3@gmail.com>
2021-04-18 23:52:33 -0300

    Declarando, atribuindo e lendo arrays

--- RelembrandoArraysDotNet/Program.cs
+++ RelembrandoArraysDotNet/Program.cs
@@ -6,7 +6,24 @@ namespace RelembrandoArraysDotNet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region declaracao_array
+            int[] a = new int[2];
+            var a2 = new int[2];
+            #endregion
+            #region atribuindo_valores
+            for (int i = 0; i < a2.Length; i++)
+            {
+                a2[i] = i*5;
+            }
+            #endregion
+            #region lendo_valores
+            Console.WriteLine(a2[1]);
+
+            foreach (var n in a2)
+            {
+                Console.WriteLine(n);
+            }
+            #endregion
         }
     }
 }
